5603a5ba64907bb96dc1182882a34443e0659bed
Pavel Kasila <pavel.kasila@gmail.com>
2021-01-01 21:32:08 +0300

    Implemented #4 feature for fallbacks, added tests for new API

--- initializer.go
+++ initializer.go
@@ -1,21 +1,24 @@
 package gormcrypto
 
 import (
-	"crypto/rsa"
 	"github.com/pkosilo/gorm-crypto/algorithms"
 	"github.com/pkosilo/gorm-crypto/serialization"
 )
 
-var Algorithm algorithms.Algorithm
-var Serializer serialization.Serializer
+var Algorithms []algorithms.Algorithm
+var Serializers []serialization.Serializer
+
+// InitWithFallbacks initializes library with fallback methods
+func InitWithFallbacks(algorithms []algorithms.Algorithm, serializers []serialization.Serializer) {
+	if len(algorithms) == 0 || len(serializers) == 0 {
+		panic("There are no algorithms and/or serializers passed!")
+	}
+
+	Algorithms = algorithms
+	Serializers = serializers
+}
 
 // Init initializes library with specified algorithm
 func Init(algorithm algorithms.Algorithm, serializer serialization.Serializer) {
-	Algorithm = algorithm
-	Serializer = serializer
-}
-
-// Deprecated: InitFromKeyPair initializes library with RSA algorithm with specified private and public keys
-func InitFromKeyPair(private *rsa.PrivateKey, public *rsa.PublicKey) {
-	Init(algorithms.NewRSA(private, public), serialization.NewJSON())
+	InitWithFallbacks([]algorithms.Algorithm{algorithm}, []serialization.Serializer{serializer})
 }
