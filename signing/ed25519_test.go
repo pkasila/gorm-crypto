d25ff3be420336599217c9b2f3e022b12221ea8d
Amy Ramos <amyanneramos@yandex.ru>
2021-01-16 20:31:11 +0300

    test: add more tests for ECDSA and Ed25519 and update previously added tests add and update tests for ECDSA and Ed25519 signing algorithms (#8 and #11)

---
+++ signing/ed25519_test.go
@@ -0,0 +1,28 @@
+package signing
+
+import (
+	"github.com/pkosilo/gorm-crypto/helpers"
+	"testing"
+)
+
+func TestECDSAEncryptDecryptCycle(t *testing.T) {
+	// Generate key pair
+	privateKey, publicKey, err := helpers.ECDSAGenerateKeyPair()
+	if err != nil {
+		t.Fatalf("Failed to generate key pair: %s\n", err.Error())
+	}
+
+	rsa := NewECDSA(privateKey, publicKey)
+
+	message := "A string... just for testing purposes :-)"
+
+	sig, err := rsa.Sign([]byte(message))
+	if err != nil {
+		t.Fatalf("Failed sign data: %s\n", err.Error())
+	}
+
+	valid, err := rsa.Verify([]byte(message), sig)
+	if err != nil || !valid {
+		t.Fatalf("Failed verify data: %s\n", err.Error())
+	}
+}
