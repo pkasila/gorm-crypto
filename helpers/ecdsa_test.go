02a87305a8e088d96c7b73337b5c5ab81d1f831a
Amy Ramos <amyanneramos@yandex.ru>
2021-01-16 20:23:13 +0300

    test: add tests for ECDSA and Ed25519 helpers some tests for features in #8, #11

---
+++ helpers/ecdsa_test.go
@@ -0,0 +1,28 @@
+package helpers
+
+import "testing"
+
+func TestRSAKeysConversion(t *testing.T) {
+	// Generate key pair
+	privateKey, publicKey, err := RSAGenerateKeyPair(4096)
+	if err != nil {
+		t.Fatalf("Failed to generate key pair: %s\n", err.Error())
+	}
+
+	// Convert to bytes
+	privBytes := RSAPrivateKeyToBytes(privateKey)
+	pubBytes, err := RSAPublicKeyToBytes(publicKey)
+	if err != nil {
+		t.Fatalf("Failed to convert public key to bytes: %s\n", err.Error())
+	}
+
+	// Convert back to rsa.PrivateKey and rsa.PublicKey
+	_, err = RSABytesToPrivateKey(privBytes)
+	if err != nil {
+		t.Fatalf("Failed to convert bytes to private key: %s\n", err.Error())
+	}
+	_, err = RSABytesToPublicKey(pubBytes)
+	if err != nil {
+		t.Fatalf("Failed to convert bytes to public key: %s\n", err.Error())
+	}
+}
