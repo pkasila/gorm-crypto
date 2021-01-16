1c7b4e3ea9728d98f69275e2ebe583f3167d4f57
Amy Ramos <amyanneramos@yandex.ru>
2021-01-16 17:43:20 +0300

    feat: add signing capabilities, ECDSA signaure algorithm implements #8

---
+++ signed_value.go
@@ -0,0 +1,56 @@
+package gormcrypto
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+)
+
+type EncryptedValue struct {
+	Raw interface{}	`json:"Raw"`
+}
+
+// Scan decrypts and deserializes value from DB, implements sql.Scanner interface
+func (j *EncryptedValue) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal value:", value))
+	}
+
+	var encValue *interface{}
+	var selectionError error
+
+	for _, algo := range Algorithms {
+		var dBytes []byte
+		dBytes, selectionError = algo.Decrypt(bytes)
+		if selectionError != nil {
+			continue
+		}
+
+		for _, serializer := range Serializers {
+			encValue, selectionError = serializer.Deserialize(dBytes)
+			if selectionError != nil {
+				continue
+			}
+		}
+	}
+
+	if selectionError != nil {
+		return selectionError
+	}
+
+	j.Raw = (*encValue).(map[string]interface{})["Raw"]
+
+	return nil
+}
+
+// Value returns serialized and encrypted value, implement driver.Valuer interface
+func (j EncryptedValue) Value() (driver.Value, error) {
+	bytes, err := Serializers[0].Serialize(j)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return Algorithms[0].Encrypt(bytes)
+}
